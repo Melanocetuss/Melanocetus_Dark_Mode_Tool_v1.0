ecc7a64b38680a7788edb249544b2ff637af40cf
Melanocetus <98456147+Melanocetuss@users.noreply.github.com>
2026-03-12 01:01:47 +0300

    update

--- content.js
+++ content.js
@@ -1,15 +1,26 @@
-// Sayfa yüklendiğinde hafızayı kontrol et
 window.addEventListener('load', () => {
+
     chrome.storage.local.get(['darkModeActive'], (result) => {
+
         if (result.darkModeActive) {
+
             applyDarkMode();
+
         }
+
     });
+
 });
 
+
 function applyDarkMode() {
+
     const s = document.createElement('style');
+
     s.id = 'cesur-force';
+
     s.innerHTML = 'html{filter:invert(1) hue-rotate(180deg)!important}img,video{filter:invert(1) hue-rotate(180deg)!important}';
+
     document.head.appendChild(s);
+
 }
